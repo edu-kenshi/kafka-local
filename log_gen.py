afcc645ab12980da4d91ed41b363ebe971c6b860
edu-kenshi <edu.kenshi@gmail.com>
2026-04-24 10:23:45 +0900

    Feat: 로그 기록 구성중

--- log_gen.py
+++ log_gen.py
@@ -16,10 +16,33 @@ if not os.path.exists(log_dir): # 없으면 -> 생성
 
 # 3. 로그 발생 및 저장
 def generate_logs():
+    # 로그 샘플 (고정된 장비 1대로 지정)
+    # 시간을 제외한 모든 값은 고정했음 -> 편의상
+    data = {
+        "timestamp" : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "sensor_id" : "AI-FACTORY-001",
+        "temperature" : 87.5,
+        "humidity"  : 42.1,
+        "status"    : "RUNNING"
+    }
+
+    # json 형태로 파일 기록(한줄에 로그 1개씩) -> dict 객체의 직렬화 처리
+    # 파일명 ./sensor_log/sensor_json.log
+    # 한줄에 JSON 객체 1개씩 문자열로 기록(JSON Lines : JSONL)
+    # 구현하시오
+
+    # text 형태로 파일 기록(한줄에 로그 1개씩) -> f-string 구성
+    # 파일명 ./sensor_log/sensor_text.log
     pass
 
 # 4. 로그 발생기 가동
 def main():
+    try:
+        while True:
+            generate_logs()
+            time.sleep(2) # 2초 대기(임의 설정)
+    except Exception:
+        print('종료 완료')
     pass
 
 # 5. 프로그램 시작
